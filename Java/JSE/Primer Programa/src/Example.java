a2651542fb08fd5f59503929c038afe1fba81da9
Xavier Roig <maroxa86@gmail.com>
2013-05-21 21:54:30 +0200

    Primer programa del libro JAVA7

---
+++ Java/JSE/Primer Programa/src/Example.java
@@ -0,0 +1,11 @@
+/*
+ * Un Sencillo programa de Java
+ * 
+ */
+public class Example {
+	//Un programa de Java comienza con la invocación de main()
+	public static void main(String[] args) {
+		System.out.println("Java drives the Web");
+	}
+
+}
