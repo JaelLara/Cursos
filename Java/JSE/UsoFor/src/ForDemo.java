ec5509f505398b5207711ae4a7d21f22122bcd14
Xavier Roig <maroxa86@gmail.com>
2013-05-22 22:27:01 +0200

    Ejemplo For

---
+++ Java/JSE/UsoFor/src/ForDemo.java
@@ -0,0 +1,14 @@
+/*
+ * Uso del bucle for
+ */
+public class ForDemo {
+	public static void main(String[] args) {
+		int count;
+		
+		for(count = 0; count < 5; count = count + 1) //Este bucle itera 5 veces
+			System.out.println("Este es la vuelta: " + count);
+
+		System.out.println("Hecho!");
+	}
+
+}
