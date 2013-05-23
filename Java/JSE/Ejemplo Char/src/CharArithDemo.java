d56ce9d88030e09df278e4c49cb8954bd1827a36
Xavier Roig <maroxa86@gmail.com>
2013-05-23 19:41:03 +0200

    Ejemplo de uso de una variable char.

---
+++ Java/JSE/Ejemplo Char/src/CharArithDemo.java
@@ -0,0 +1,18 @@
+// Las variables de caracteres se pueden procesar como enteros
+public class CharArithDemo {
+	public static void main(String[] args) {
+		char ch;
+		
+		ch = 'X';
+		
+		System.out.println("ch contiene " + ch);
+		
+		ch++; //incremento ch
+		
+		System.out.println("ch ahora es " + ch);
+		
+		ch = 90;
+		System.out.println("ch ahora es " + ch);
+	}
+
+}
