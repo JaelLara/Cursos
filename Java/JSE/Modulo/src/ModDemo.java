448ca2582048001aef08d8849b046644521db05c
Xavier <maroxa86@gmail.com>
2013-05-25 12:58:02 +0200

    Ejemplo de uso del modulo de una división

---
+++ Java/JSE/Modulo/src/ModDemo.java
@@ -0,0 +1,18 @@
+// El operador %
+public class ModDemo {
+	public static void main(String[] args) {
+		int iresult, irem;
+		double dresult, drem;
+		
+		iresult = 10 / 3;
+		irem = 10 % 3;
+		
+		dresult = 10.3 / 3.0;
+		drem = 10.3 % 3.0;
+		
+		System.out.println("Resultado y resto de 10 / 3: " + iresult + " " + irem);
+		
+		System.out.println("Resultado y resto de 10.3 / 3.0: " + dresult + " " + drem);
+	}
+
+}
