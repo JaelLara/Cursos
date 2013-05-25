942363d9b1435ece3e69bd2f477898b103034569
Xavier <maroxa86@gmail.com>
2013-05-25 12:31:01 +0200

    Ejemplo de inicialización dinámica

---
+++ Java/JSE/Inicialización dinámica/src/DynInit.java
@@ -0,0 +1,11 @@
+// Ilustra la inicialización dinámica
+public class DynInit {
+	public static void main(String[] args) {
+		double radio = 4, altura = 5;
+		
+		//inicializar el volumen de forma dinámica
+		double volumen = 3.1416 * radio * altura;
+		System.out.println("El volumen es: " + volumen);
+	}
+
+}
