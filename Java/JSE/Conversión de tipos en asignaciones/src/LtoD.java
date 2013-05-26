d3e25b55b8601c174c7988ad8c13ba20555e1a6d
Xavier <maroxa86@gmail.com>
2013-05-26 09:17:47 +0200

    Ejemplo de conversión de datos compatibles

---
+++ Java/JSE/Conversión de tipos en asignaciones/src/LtoD.java
@@ -0,0 +1,13 @@
+//Conversión automática de long a double
+public class LtoD {
+	public static void main(String[] args) {
+		long L;
+		double D;
+		
+		L = 100123285L;
+		D = L;
+
+		System.out.println(" L y D: " + L + " " + D);
+	}
+
+}
